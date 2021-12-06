6f84008e0cfd88f7e3e2b10b832b6811835e0500
Alex Finnarn <alex.finnarn@gmail.com>
2021-12-05 22:17:14 -0600

    try adding watcher as worker

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
+release: ./scripts/release-tasks.sh
 web: vendor/bin/heroku-php-apache2 public
+worker: yarn encore dev-server
---
+++ scripts/release-tasks.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Install site if on review app. Import config if on other environments.
+if [[ -z "$HEROKU_PR_NUMBER" ]]
+  then
+  echo "Not on a review app. No need to copy database."
+else
+  # Split db url into parts since the pg:copy command needs it to confirm deletion.
+  # ${DB_NAME[3]} ends up being the database name.
+  # IFS is the internal string separator which needs set to forward-slash for parsing db url string.
+  IFS='/' read -r -a DB_NAME <<< "$DATABASE_URL"
+
+  echo "Review app detected. Copying database from staging..."
+#  heroku pg:reset -a $HEROKU_APP_NAME --confirm $HEROKU_APP_NAME
+#  heroku pg:copy digital-dash-stage-adv-cu::DATABASE_URL $DATABASE_URL -a $HEROKU_APP_NAME --confirm ${DB_NAME[3]}
+
+  # Attach Cloudinary from production.
+#  CL_URL=$(heroku config:get CLOUDINARY_URL -a digital-dash-adv-cu)
+#  /app/vendor/bin/drush state:set cua_cloudinary.cloudinary_url ${CL_URL} --input-format=string
+
+
+  echo "Triggering test run...at https://digital-dash-stage-adv-cu.herokuapp.com/api/review-apps/${HEROKU_APP_NAME}/${HEROKU_BRANCH}"
+#  curl https://digital-dash-stage-adv-cu.herokuapp.com/api/review-apps/${HEROKU_APP_NAME}/${HEROKU_BRANCH}
+
+fi
